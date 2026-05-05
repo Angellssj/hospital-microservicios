50ddd4b52cd26615f9fe0788b22531c37a51b1f9
Eldaridin <dario.rojas.monsalvez@gmail.com>
2026-05-05 18:22:14 -0400

    Auth
    Añadi el servicio Auth

--- patient-service/src/main/java/com/hospital/patient_service/model/Paciente.java
+++ patient-service/src/main/java/com/hospital/patient_service/model/Paciente.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "pacientes")
 public class Paciente {
-
+    // Atributos
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
